b62a88dd49da9dfa94a3fffca906cffaf9d01379
hm5961 <71277818+hm5961@users.noreply.github.com>
2021-09-26 06:32:02 +0900

    Add files via upload

--- chap4/ex4_4.java
+++ chap4/ex4_4.java
@@ -2,4 +2,23 @@ package chap4;
 
 public class ex4_4 {
 
+	String title;
+	String author;
+	
+	public ex4_4(String t) {
+		title = t; author = "작자미상";
+	}
+	
+	public ex4_4(String t, String a) {
+		title = t; author = a;
+	}
+	
+	public static void main(String [] args) 
+	{
+		ex4_4 littlePrince = new ex4_4("어린왕자", "생텍쥐페리");
+		ex4_4 loveStory = new ex4_4("춘향전");
+		
+		System.out.println(littlePrince.title + " " + littlePrince.author);
+		System.out.println(loveStory.title + " " + loveStory.author);
+	}
 }
--- chap4/ex4_5.java
+++ chap4/ex4_5.java
@@ -1,5 +1,34 @@
 package chap4;
 
 public class ex4_5 {
-
+	String title;
+	String author;
+	
+	void show() { 
+		System.out.println(title + " " + author); 
+	}
+	
+	public ex4_5() 
+	{
+		this("", "");
+		System.out.println("생성자 호출됨");
+	}
+	
+	public ex4_5(String title) {
+		
+		this(title, "작자미상");
+	}
+	
+	public ex4_5(String title, String author) {
+		
+		this.title = title; this.author = author;
+	}
+	
+	public static void main(String [] args) 
+	{
+		ex4_5 littlePrince = new ex4_5("어린왕자", "생텍쥐페리");
+		ex4_5 bible = new ex4_5("Bible");
+		ex4_5 emptyBook = new ex4_5();
+		bible.show();
+	}
 }
