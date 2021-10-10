16800a635b61747fcc2d9ebe30b74fa4783f900c
hm5961 <71277818+hm5961@users.noreply.github.com>
2021-10-10 23:22:43 +0900

    Add files via upload

---
+++ chap5/prac02.java
@@ -0,0 +1,56 @@
+package chap5;
+
+class TV 
+{
+	private int size;
+
+	public TV(int size) 
+	{
+		this.size = size;
+	}
+
+	protected int getSize() 
+	{
+		return size;
+	}
+}
+
+class ColorTV extends TV 
+{
+	private int color;
+
+	ColorTV(int size, int color) 
+	{
+		super(size);
+		this.color = color;
+	}
+
+	public void printProperty() 
+	{
+		System.out.print( getSize() + "인치 " + color + "컬러");
+	}
+}
+
+class IPTV extends ColorTV 
+{
+	String IP;
+
+	IPTV(String IP, int size, int resolution) 
+	{
+		super(size, resolution);
+		this.IP = IP;
+	}
+
+	public void printProperty() 
+	{
+		System.out.print("나의 IPTV는 " + IP + " 주소의 ");
+		super.printProperty();
+	}
+}
+
+public class prac02 {
+	public static void main(String[] args) {
+		IPTV iptv = new IPTV("192.1.1.2", 32, 2048);
+		iptv.printProperty();
+	}
+}
