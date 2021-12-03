d580a69884cbfab010523e92f3af92600f06e08e
hm5961 <71277818+hm5961@users.noreply.github.com>
2021-12-04 03:46:04 +0900

    전부삭제 + ID 영+숫자 합

--- receive_and_send_RNG.java
+++ receive_and_send_RNG.java
@@ -37,10 +37,31 @@ public class receive_and_send_RNG {
 		}
 		int i;
 		//생성 수 조정
-		for (i = 0; i <= 1000; i++) 
+		for (i = 0; i <= 50; i++) 
 		{
 
-			int k = (int) (Math.random() * 9999 + 1000);
+			int k = (int) (Math.random() * 8999 + 1000);
+			
+			String ks = "";
+			int englen =(int) (Math.random() * 7 + 1);
+			int numlen =(int) (Math.random() * 8 + 2 );
+			
+			String Eng;
+			String Num;
+			
+			for( int j = 0; j<englen; j++ )
+			{
+				char engran = (char)(Math.random()*26+97);
+				if(ks == null)
+					ks += engran;
+				else
+					ks += engran;
+			}
+			for( int j = 0; j<numlen-englen; j++ )
+			{
+				char numran = (char)(Math.random()*10+48);
+				ks += numran;
+			}
 			// receiver 가 test
 			if (k % 2 == 0) 
 			{
@@ -48,14 +69,39 @@ public class receive_and_send_RNG {
 				PreparedStatement pstmt = con.prepareStatement(sql);
 
 				pstmt.setString(1, "test");
-				pstmt.setInt(2, k);
+				pstmt.setString(2, ks);
 
 				Date now = Calendar.getInstance().getTime();
-				SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+				SimpleDateFormat formatter = new SimpleDateFormat("yy.MM.dd HH:mm");
 				String formatedNow = formatter.format(now);
 				pstmt.setString(3, formatedNow);
+				String context = null;
+				
 
-				String context = "안녕하세요. 저는 " + k + "입니다";
+				
+				
+				
+				
+				if( k % 10 == 2)
+				{
+					context = "안녕하세요. 저는 " + ks + "에요";
+				}
+				else if( k % 10 == 4)
+				{
+					context = "반갑습니다. 저는 " + ks + "입니다";
+				}
+				else if( k % 10 == 6)
+				{
+					context = "제 아이디는 " + ks + "에요. 잘부탁드립니다.";
+				}
+				else if( k % 10 == 8)
+				{
+					context = "안녕하세요. 매칭 결과보고 연락드렸습니다. 제 아이디는 " + ks + "입니다.";
+				}
+				else if( k % 10 == 0)
+				{
+					context = "혹시 룸메이트 구하셨나요? " + ks + "로 답장 부탁드려요.";
+				}
 				pstmt.setString(4, context);
 				pstmt.setInt(5, -1);
 
@@ -71,11 +117,11 @@ public class receive_and_send_RNG {
 
 				// sender가 test
 
-				pstmt.setInt(1, k);
+				pstmt.setString(1, ks);
 				pstmt.setString(2, "test");
 
 				Date now = Calendar.getInstance().getTime();
-				SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+				SimpleDateFormat formatter = new SimpleDateFormat("yy.MM.dd HH:mm");
 				String formatedNow = formatter.format(now);
 				pstmt.setString(3, formatedNow);
 
